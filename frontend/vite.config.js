3c6c90c4858f6f48aa971efddefa164f8f369d8f
unknown <ezekuiel100@gmail.com>
2025-04-16 15:14:13 -0300

    remove custom server configuration from Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync("./localhost-key.pem"),
-      cert: fs.readFileSync("./localhost.pem"),
-    },
-    host: "localhost", // Garante que está rodando corretamente
-    port: 5174, // Ou a porta que você estiver usando
-  },
 });
